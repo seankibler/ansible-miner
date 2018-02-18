a777bb04d778b4c6dbda9f52f4b502719c7d211b
Sean Kibler <skibler1223@gmail.com>
2018-02-18 15:09:19 -0500

    install and report metrics to datadog

--- roles/common/files/html/temperatures.php
+++ roles/common/files/html/temperatures.php
@@ -1,20 +1,32 @@
 <?php
+
+# Get CPU data
 $output = [];
 
 exec("sensors -A coretemp-isa-0000", $output);
 
 array_shift($output);
+array_pop($output);
 
-$cpu_temps = array_map(function($core_temp) {
-	return trim(str_replace("+", "", preg_split("/[\s]+/", $core_temp)[2])); 
-}, $output);
+$cpu_temps = [];
+for ($x = 0; $x < count($output); $x++) {
+    $m = [];
+    preg_match("/[\d]+\.[\d]+/", $output[$x], $m);
 
+    $cpu_temps[$x] = $m[0];
+};
+
+# Get GPU data
 $gpu_temps = [];
 exec("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader", $gpu_temps);
 
-array_pop($cpu_temps);
-
+# Return as JSON
+$data = [
+    'cpu' => [
+        'cores' => $cpu_temps
+    ],
+    'gpu' => $gpu_temps
+];
 header('Content-Type: application/json');
-
-echo json_encode(['cpu' => ['cores' => $cpu_temps], 'gpu' => $gpu_temps]);
+echo json_encode($data, JSON_FORCE_OBJECT);
 ?>
