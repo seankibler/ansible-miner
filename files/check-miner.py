d0e19decba998ad1e59645fda03ebb240bd7c14f
Sean Kibler <skibler@illuminateed.net>
2019-08-10 23:31:39 -0400

    Refactors playbook, xmr-stak role and scripts
    - Refactors scripts including autodetection of GPUs
    - Adds teamredminer and control script
    - Moves unrelated files and tasks from xmr-stak role to playbook
    - Restores and updates installation of xmr-stak configs

--- files/check-miner.py
+++ files/check-miner.py
@@ -1,14 +1,21 @@
 import json
+import requests
+import re
 from urllib2 import Request, urlopen, URLError
 
-miner_api = "http://localhost:10000/api.json"
+miner_api = "https://web.xmrpool.eu:8119/stats_address?address=4ARdAsVF86DTeWFjZEsd2KFBwjozisRuMiBiqyp9tnM6XLsAKCoXpMWAfCM4SehcxV1NDzgBKXZLoGApEYaQ5NVmUDgHqak"
 statuscake_push_url = "https://push.statuscake.com/?PK=07e2a55247bbe50&TestID=4022995&time=0"
 
 try:
-	miner_api_response = urlopen(miner_api)
-	miner_api_data = json.loads(miner_api_response.read())
-	if miner_api_data['hashrate']['total'][0] > 1500:
-		statuscake_response = urlopen(statuscake_push_url)
+	miner_api_response = requests.get(miner_api, verify=False)
+	miner_api_data = json.loads(miner_api_response.content)
+	hashrate = re.findall('\d+\.\d+', miner_api_data['stats']['hashrate'])
+
+	if hashrate != None:
+		hashrate = float(hashrate[0])
+
+	if hashrate >= 2.4:
+		statuscake_response = requests.get(statuscake_push_url)
 except URLError as e:
 	if hasattr(e, 'reason'):
 		print 'Failed to reach server. Reason: ', e.reason
